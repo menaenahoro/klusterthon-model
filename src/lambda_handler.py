8545717b2b12a6f4aafe3fb6124a1d4084b5908a
Elomena Enahoro <menaenahoro@gmail.com>
2023-11-27 05:34:01 +0100

    update

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -38,12 +38,9 @@ def lambda_handler(event, context):
         return dict(message="successful", error=False, data=[result])
 
     if url_for=="model_service":
-        try:
-            result = model_service.run_main_process(user_id=user_id, course_id=course_id)
-            # Model to process function
-            return dict(message="successful", error=False, data=result)
-        except Exception as e:
-            return dict(error=True, message=f'error: {e}')
+        result = model_service.run_main_process(user_id=user_id, course_id=course_id)
+        # Model to process function
+        return dict(message="successful", error=False, data=result)
 
     if url_for=="event_bridge":
         try:
